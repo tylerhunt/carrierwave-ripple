26151dbdae6b3ed8b28cab0c6a2e8b7ae54b5afa
Tyler Hunt <tyler@tylerhunt.com>
2011-12-07 16:38:09 -0800

    Generate Gem.

---
+++ lib/carrierwave-ripple.rb
@@ -0,0 +1,7 @@
+require "carrierwave-ripple/version"
+
+module Carrierwave
+  module Ripple
+    # Your code goes here...
+  end
+end
---
+++ lib/carrierwave-ripple/version.rb
@@ -0,0 +1,5 @@
+module Carrierwave
+  module Ripple
+    VERSION = "0.0.1"
+  end
+end
